53651e565cafa9ed6b74a9fa0958dc16d0bfab68
Jeferson Florez <jeffersonflorez73@gmail.com>
2025-04-25 17:52:33 -0500

    Agrenado controller de Appointment

--- src/main/java/edu/unimag/consultoriomedico/controller/AppointmentController.java
+++ src/main/java/edu/unimag/consultoriomedico/controller/AppointmentController.java
@@ -1,9 +1,15 @@
 package edu.unimag.consultoriomedico.controller;
 
+import edu.unimag.consultoriomedico.dto.AppointmentDTO;
+import edu.unimag.consultoriomedico.dto.PatientDTO;
 import edu.unimag.consultoriomedico.service.AppointmentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/appointments")
@@ -11,5 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
+
+    @PostMapping
+    public ResponseEntity<AppointmentDTO> createAppointment(@Valid @RequestBody AppointmentDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.createAppointment(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<AppointmentDTO>> getAllAppointments() {
+        return ResponseEntity.ok(appointmentService.getAllAppointments());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppointmentDTO> getAppointmentById(@PathVariable Long id) {
+        return ResponseEntity.ok(appointmentService.getAppointmentById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AppointmentDTO> updateAppointment(@PathVariable Long id, @Valid @RequestBody AppointmentDTO dto) {
+        return ResponseEntity.ok(appointmentService.updateAppointment(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppointment(@PathVariable Long id) {
+        appointmentService.deleteAppointment(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
