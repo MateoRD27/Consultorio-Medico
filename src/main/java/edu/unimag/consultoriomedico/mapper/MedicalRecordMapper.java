a22e0dfcf89fec230fd1f249118cf1f5b949c58e
Jeferson Florez <jeffersonflorez73@gmail.com>
2025-04-24 21:18:41 -0500

    Agregando la excepcion NoDataFoundException

--- src/main/java/edu/unimag/consultoriomedico/mapper/MedicalRecordMapper.java
+++ src/main/java/edu/unimag/consultoriomedico/mapper/MedicalRecordMapper.java
@@ -7,12 +7,12 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MedicalRecordMapper {
- /*  @Mapping(source = "appointment.id", target = "appointmentId")
+    @Mapping(source = "appointment.id", target = "appointmentId")
     @Mapping(source = "patient.id", target = "patientId")
 
     MedicalRecordDTO toDTO(MedicalRecord medicalRecord);
 
     @Mapping(target = "appointment", ignore = true)
     @Mapping(target = "patient", ignore = true)
-    MedicalRecord toEntity(MedicalRecordDTO medicalRecordDTO);*/
+    MedicalRecord toEntity(MedicalRecordDTO medicalRecordDTO);
 }
