d3ecdcfd95553c492f151ee14d7b0ce4fd6240b0
Mateo RD <tu-correo@example.com>
2025-04-25 17:18:11 -0500

    agregando el controlador de Doctor y las clases de los controladores

---
+++ src/main/java/edu/unimag/consultoriomedico/controller/DoctorController.java
@@ -0,0 +1,50 @@
+package edu.unimag.consultoriomedico.controller;
+
+import edu.unimag.consultoriomedico.dto.DoctorDTO;
+import edu.unimag.consultoriomedico.service.DoctorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
+@RequiredArgsConstructor
+public class DoctorController {
+    private final DoctorService doctorService;
+
+    @GetMapping
+    public ResponseEntity<List<DoctorDTO>> getAllDoctors() {
+        return ResponseEntity.ok(doctorService.getAllDoctors());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorDTO> getDoctorById(@PathVariable Long id) {
+        return ResponseEntity.ok(doctorService.getDoctorById(id));
+    }
+
+    @GetMapping
+    ResponseEntity<List<DoctorDTO>> getDoctorsBySpecialty(@PathVariable String specialty) {
+        return ResponseEntity.ok(doctorService.getDoctorsBySpecialty(specialty));
+    }
+
+    @PostMapping
+    public ResponseEntity<DoctorDTO> createDoctor(@Valid @RequestBody DoctorDTO doctorDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(doctorService.createDoctor(doctorDto));
+    }
+
+    @PutMapping
+    public ResponseEntity<DoctorDTO> updateDoctor(@PathVariable Long id, @Valid @RequestBody DoctorDTO doctorDto) {
+        return ResponseEntity.ok(doctorService.updateDoctor(id, doctorDto));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
+        doctorService.deleteDoctor(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
