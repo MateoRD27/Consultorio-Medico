6ab015f8b0fb7715e0859fb93efc3683c39fc998
Jeferson Florez <jeffersonflorez73@gmail.com>
2025-05-30 10:10:03 -0500

    Ajustando mapperConsultorioRoom

--- src/main/java/edu/unimag/consultoriomedico/mapper/ConsultRoomMapper.java
+++ src/main/java/edu/unimag/consultoriomedico/mapper/ConsultRoomMapper.java
@@ -2,10 +2,23 @@ package edu.unimag.consultoriomedico.mapper;
 
 import edu.unimag.consultoriomedico.dto.ConsultRoomDTO;
 import edu.unimag.consultoriomedico.entity.ConsultRoom;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface ConsultRoomMapper {
+
+    @Mappings({
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "name", target = "name"),
+            @Mapping(source = "roomNumber", target = "roomNumber"),
+            @Mapping(source = "floor", target = "floor"),
+            @Mapping(source = "description", target = "description")
+    })
     ConsultRoomDTO toDTO(ConsultRoom consultRoom);
-    ConsultRoom toEntity(ConsultRoomDTO consultRoomDTO);
+
+    @InheritInverseConfiguration
+    ConsultRoom toEntity(ConsultRoomDTO dto);
 }
